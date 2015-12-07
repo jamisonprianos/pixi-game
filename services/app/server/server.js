9c6be5477add98ec5b9d387d884ea86f3945d6ca
Jamison Prianos <jamison.prianos@gmail.com>
2015-12-07 07:59:06 -0500

    updates

--- services/app/server/server.js
+++ services/app/server/server.js
@@ -4,27 +4,67 @@ var uglify = require('uglify-js');
 var sass = require('node-sass');
 var htmlMinifier = require('html-minifier');
 
+var cache = {
+  css: undefined,
+  js: undefined
+};
+
+var cacheAssets = false;
+var compressAssets = false;
+
+var jsFiles = ['./source/js/pixi.multisprite.js', './source/js/main.js'];
+
+function concatFiles(jsFiles) {
+  if (jsFiles.length === 0) {
+    return '';
+  } else {
+    var fileName = jsFiles.shift();
+    console.log(fileName);
+    return fs.readFileSync(fileName, {encoding: 'utf8'}) + '\n' + concatFiles(jsFiles);
+  }
+}
+
 app.get('/app.js', function (req, res) {
   console.log('javascript request');
-  var uglified = uglify.minify(['./source/js/pixi.min.js', './source/js/main.js']);
-  res.writeHead(200, {'content-type': 'application/javascript'});
-  res.end(uglified.code);
+  if (cache.js) {
+    res.writeHead(200, {'content-type': 'application/javascript'});
+    res.end(cache.js);
+  } else {
+    if (compressAssets) {
+      var uglified = uglify.minify(jsFiles);
+    } else {
+      var uglified = { code: concatFiles(JSON.parse(JSON.stringify(jsFiles))) };
+    }
+    res.writeHead(200, {'content-type': 'application/javascript'});
+    res.end(uglified.code);
+    if (cacheAssets) {
+      cache.js = uglified.code;
+    }
+  }
 });
 
 app.get('/app.css', function (req, res) {
   console.log('css request');
-  sass.render({
-    file: './source/scss/main.scss',
-    outputStyle: 'compressed'
-  }, function(err, result) {
-    if (err) {
-      res.writeHead(500, {'content-type': 'text/plain'});
-      res.end('HTTP/1.1 500 Internal Server Error');
-    } else {
-      res.writeHead(200, {'content-type': 'text/css'});
-      res.end(result.css);
-    }
-  });
+  if (cache.css) {
+    res.writeHead(200, {'content-type': 'text/css'});
+    res.end(cache.css);    
+  } else {
+    sass.render({
+      file: './source/scss/main.scss',
+      outputStyle: 'compressed'
+    }, function(err, result) {
+      if (err) {
+        res.writeHead(500, {'content-type': 'text/plain'});
+        res.end('HTTP/1.1 500 Internal Server Error');
+      } else {
+        res.writeHead(200, {'content-type': 'text/css'});
+        res.end(result.css);
+        if (cacheAssets) {
+          cache.css = result.css;
+        }
+      }
+    });
+  }
 });
 
 app.get(/^(.+)$/, function(req, res){ 
@@ -34,7 +74,7 @@ app.get(/^(.+)$/, function(req, res){
   fs.exists(fullFilename, function (exists) {
     if (exists) {
       var ext = fullFilename.split('.').pop();
-      var encoding = ext === 'html' ? { encoding: 'utf8' } : {};
+      var encoding = (ext === 'html' || ext === 'js') ? { encoding: 'utf8' } : {};
       fs.readFile(fullFilename, encoding, function (err, contents) {
         if (err) {
           res.writeHead(500, {'content-type': 'text/plain'});
@@ -51,8 +91,14 @@ app.get(/^(.+)$/, function(req, res){
             });
             res.writeHead(200, {'content-type': 'text/html'});
             res.end(output);
-          } else if (ext === 'jpg') {
+          } else if (ext === 'jpg' || ext == 'jpeg') {
             res.writeHead(200, {'content-type': 'image/jpeg'});
+            res.end(contents);
+          } else if (ext === 'png') {
+            res.writeHead(200, {'content-type': 'image/png'});
+            res.end(contents);
+          } else if (ext === 'js') {
+            res.writeHead(200, {'content-type': 'application/javascript'});
             res.end(contents);
           }
         }
--- services/app/server/source/js/main.js
+++ services/app/server/source/js/main.js
@@ -1,24 +1,70 @@
-/*
-var player;
-var lastTimestamp;
-var angle = 0;
-var keyMap = {
-  87: 'up',
-  65: 'left',
-  68: 'right',
-  83: 'down',
-  49: 'action1'
+var _resources;
+var _stage;
+
+function getFramesFromSpriteSheet(texture, frameWidth, frameHeight, row) {
+  row = row || 0;
+  var frames = [];
+  for(var i = 0; i < texture.width-frameWidth; i+=frameWidth) {
+    frames.push(new PIXI.Texture(texture.baseTexture, new PIXI.Rectangle(i, row * frameHeight, frameWidth, frameHeight)));
+  }
+  return frames;
 }
-chargeMax = 2;
-chargeTime = 0;
-var keys = {};
-Object.keys(keyMap).forEach(function (kmk) {
-  keys[keyMap[kmk]] = false;
-});
+
+function Character(name, spriteSheet) {
+  PIXI.extras.MovieClip.call(this, getFramesFromSpriteSheet(_resources.character.texture, 70, 70, 0));
+  this.animationId = 0;
+  return this;
+};
+
+Character.prototype = Object.create( PIXI.extras.MovieClip.prototype );
+Character.prototype.constructor = Character;
+
+Character.prototype.setAnimation = function (id) {
+  if (this.animationId !== id) {
+    console.log('Changing animation to ' + id);
+    this.animationId = id;
+    this.textures = getFramesFromSpriteSheet(_resources.character.texture, 70, 70, id);
+    this.gotoAndPlay(0);
+  }
+}
+
+// Character.prototype.setAnimation = function (row) {
+//   var self = this;
+//   self.animation = new PIXI.extras.MovieClip(getFramesFromSpriteSheet(_resources[self.name].texture, 70, 70, row))
+//   console.log(self.animation);
+//   self.animation.anchor = this.anchor;
+//   self.animation.animationSpeed = this.animationSpeed;
+//   self.animation.position = this.position;
+//   self.animation.gotoAndPlay(0);
+//   _stage.addChild(self.animation);
+// };
+
+// Character.prototype.play = function () {
+//   this.animation.play();
+// }
+
+// Character.prototype.stop = function () {
+//   this.animation.stop();
+// }
+
 
 $(function () {
-  player = $('<i></i>').addClass('fa fa-arrow-circle-right fa-2x player');
-  player.appendTo($('body'));
+
+  var character;
+  var angle = 0;
+  var keyMap = {
+    87: 'up',
+    65: 'left',
+    68: 'right',
+    83: 'down',
+    49: 'action1'
+  }
+  chargeMax = 2;
+  chargeTime = 0;
+  var keys = {};
+  Object.keys(keyMap).forEach(function (kmk) {
+    keys[keyMap[kmk]] = false;
+  });
 
   $('body').on('keydown', function (e) {
     console.log(e.keyCode);
@@ -37,13 +83,53 @@ $(function () {
     }
   });
 
-  lastTimestamp = performance.now();
+  var lastTimestamp = performance.now();
 
-  var step = function (timestamp) {
+  var renderer = new PIXI.autoDetectRenderer(800, 600, {
+    view: document.getElementById('game')
+  });
+  document.body.appendChild(renderer.view);
+  _stage = new PIXI.Container();
+
+  //character = new Character('character', 'images/character.png');
+
+  PIXI.loader.add('character', 'images/character.png').load(function (loader, resources) {
+    _resources = resources;
+    character = new PIXI.AnimatedSprite({
+      walkDown: getFramesFromSpriteSheet(_resources.character.texture, 70, 70, 0),
+      walkLeft: getFramesFromSpriteSheet(_resources.character.texture, 70, 70, 1),
+      walkRight: getFramesFromSpriteSheet(_resources.character.texture, 70, 70, 2),
+      walkUp: getFramesFromSpriteSheet(_resources.character.texture, 70, 70, 3),
+      walkDownLeft: getFramesFromSpriteSheet(_resources.character.texture, 70, 70, 4),
+      walkUpLeft: getFramesFromSpriteSheet(_resources.character.texture, 70, 70, 5),
+      walkDownRight: getFramesFromSpriteSheet(_resources.character.texture, 70, 70, 6),
+      walkUpRight: getFramesFromSpriteSheet(_resources.character.texture, 70, 70, 7)
+    }, 8, 'walkRight');
+    character.loop = true;
+    character.play();
+    character.anchor = { x: 0.5, y: 0.8 };
+    character.animationSpeed = 0.1;
+    character.gotoAndPlay(0);
+    character.position = { x: 400, y: 300 };
+    _stage.addChild(character);
+    animate();
+  });
+
+  var backTexture = PIXI.Texture.fromImage('images/grass.png');
+  var back = new PIXI.TilingSprite(backTexture, 1300, 694);
+  back.position = { x: 0, y: 0 };
+  back.tilePosition = { x: 0, y: 0 };
+  _stage.addChild(back);
+
+  function animate(timestamp) {
+
     var delta = timestamp - lastTimestamp;
     var fps = (1000 / delta).toFixed(1) + ' FPS';
     $('#fps').text(fps);
     lastTimestamp = timestamp;
+
+    requestAnimationFrame(animate);
+
     if (keys.left) {
       angle -= delta / 200;
     }
@@ -51,51 +137,42 @@ $(function () {
       angle += delta / 200;
     }
     angle = angle % (2 * Math.PI);
-    $('.player').css('-webkit-transform', 'rotate(' + (angle * 180 / Math.PI) + 'deg)');
+    //$('.player').css('-webkit-transform', 'rotate(' + (angle * 180 / Math.PI) + 'deg)');
     $('#angle').text(angle.toFixed(1));
-    if (keys.action1) {
-      chargeTime += delta;
+    // if (keys.action1) {
+    //   chargeTime += delta;
+    // } else {
+    //   chargeTime = 0;
+    // }
+    // if (chargeTime > chargeMax * 1000) { chargeTime = chargeMax * 1000; }
+    // $('#chargeFill').css('width', (chargeTime / 10 / chargeMax) + '%');
+    if (angle < Math.PI / 4.0) { // || angle >= Math.PI * 7.0 / 4.0) {
+      character.currentSequence = 'walkRight';
     } else {
-      chargeTime = 0;
+      character.currentSequence = 'walkDown';
     }
-    if (chargeTime > chargeMax * 1000) { chargeTime = chargeMax * 1000; }
-    $('#chargeFill').css('width', (chargeTime / 10 / chargeMax) + '%');
     if (!(keys.up && keys.down)) {
       if (keys.up) {
-        $('.player').css('top', '+=' + (delta * Math.sin(angle) / 7) + 'px')
-        $('.player').css('left', '+=' + (delta * Math.cos(angle) / 7) + 'px')
+        character.play();
+        character.position.y += (delta * Math.sin(angle) / 7);
+        character.position.x += (delta * Math.cos(angle) / 7);
+      } else if (keys.down) {
+        character.play();
+        character.position.y -= (delta * Math.sin(angle) / 9);
+        character.position.x -= (delta * Math.cos(angle) / 9);
+      } else {
+        character.stop();
       }
-      if (keys.down) {
-        $('.player').css('top', '-=' + (delta * Math.sin(angle) / 9) + 'px')
-        $('.player').css('left', '-=' + (delta * Math.cos(angle) / 9) + 'px')
-      }
+    } else {
+      character.stop();
     }
-    window.requestAnimationFrame(step);
-  };
+    character.advanceTime(delta);
 
-  window.requestAnimationFrame(step);
-
-});
-*/
-
-$(function () {
-  var renderer = new PIXI.autoDetectRenderer(800, 600, {
-    view: document.getElementById('game')
-  });
-  document.body.appendChild(renderer.view);
-  var stage = new PIXI.Container();
-
-  var backTexture = PIXI.Texture.fromImage('images/background-forest.jpg');
-  var back = new PIXI.TilingSprite(backTexture, 1300, 694);
-  back.position = { x: 0, y: 0 };
-  back.tilePosition = { x: 0, y: 0 };
-  stage.addChild(back);
-
-  function animate() {
-    requestAnimationFrame(animate);
-    back.tilePosition.x -= .25;
-    renderer.render(stage);
+    renderer.render(_stage);
   }
 
-  animate();
+  //animate();
+
+
+
 });
